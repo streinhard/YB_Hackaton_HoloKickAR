ebfaed0842f964d5f70a19808c05b49d941b288a
Luca Tescari <luca.tescari@bitforge.ch>
2019-11-15 16:30:53 +0100

    push ze lörp

--- YBUnity/Assets/Prefabs/PlayerController/PlayerController.cs
+++ YBUnity/Assets/Prefabs/PlayerController/PlayerController.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
+    // public float speed = 10f;
     public Vector3 targetPos;
     public bool isMoving;
     const int MOUSE = 0;
+    
+    // Lerpyderpy stuff
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // Movement speed in units per second.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+    
+    // Total distance between the markers.
+    private float journeyLength;
     
     // Start is called before the first frame update
     void Start()
@@ -16,11 +30,26 @@ public class PlayerController : MonoBehaviour
         isMoving = false;
         
         Cursor.visible = false;
+        
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed equals current distance divided by total distance.
+        float fractionOfJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
+        
         /*
             Code for locking cursor
          
