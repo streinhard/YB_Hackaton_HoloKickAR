78e57b7eeab65eaf9734a0ae8b6cdc9c3fb1eacc
Luca Tescari <luca.tescari@bitforge.ch>
2019-11-15 15:20:32 +0100

    disable mouse controlling of player

--- YBUnity/Assets/Prefabs/PlayerController/PlayerController.cs
+++ YBUnity/Assets/Prefabs/PlayerController/PlayerController.cs
@@ -38,33 +38,39 @@ public class PlayerController : MonoBehaviour
         */
         
       
+        // Code to set position to mouse position
+        /*
         SetTarggetPosition();
         
         if(isMoving)
         {
             MoveObject();
         }
+        */
         
-        void SetTarggetPosition()
-        {
-            Plane plane = new Plane(Vector3.up,transform.position);
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float point = 0f;
+        
+        
+    }
+    
+    void SetTarggetPosition()
+    {
+        Plane plane = new Plane(Vector3.up,transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float point = 0f;
  
-            if(plane.Raycast(ray, out point))
-                targetPos = ray.GetPoint(point);
+        if(plane.Raycast(ray, out point))
+            targetPos = ray.GetPoint(point);
          
-            isMoving = true;
-        }
-        void MoveObject()
-        {
-            transform.LookAt(targetPos);
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        isMoving = true;
+    }
+    void MoveObject()
+    {
+        transform.LookAt(targetPos);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
  
-            if (transform.position == targetPos)
-                isMoving = false;
-            Debug.DrawLine(transform.position,targetPos,Color.red);
+        if (transform.position == targetPos)
+            isMoving = false;
+        Debug.DrawLine(transform.position,targetPos,Color.red);
  
-        }
     }
 }
