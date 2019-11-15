36385eecf086aefd6d96784646ded91c729361f9
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2019-11-15 16:38:22 +0100

    Fun with flags... and branding

--- YBUnity/Assets/Prefabs/PlayerController/PlayerController.cs
+++ YBUnity/Assets/Prefabs/PlayerController/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public Vector3 targetPos;
     public bool isMoving;
     const int MOUSE = 0;
-    
+
     // Lerpyderpy stuff
     // Transforms to act as start and end markers for the journey.
     public Transform startMarker;
@@ -19,18 +19,18 @@ public class PlayerController : MonoBehaviour
 
     // Time when the movement started.
     private float startTime;
-    
+
     // Total distance between the markers.
     private float journeyLength;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         targetPos = transform.position;
         isMoving = false;
-        
+
         Cursor.visible = false;
-        
+
         // Keep a note of the time the movement started.
         startTime = Time.time;
 
@@ -49,57 +49,47 @@ public class PlayerController : MonoBehaviour
 
         // Set our position as a fraction of the distance between the markers.
         transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
-        
-        /*
-            Code for locking cursor
-         
-            if (Input.GetKeyDown("1"))
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-            
-            if (Input.GetKeyDown("2"))
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-        */
-        
-      
+
+        if (Input.GetKeyDown("1"))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
         // Code to set position to mouse position
-        /*
         SetTarggetPosition();
-        
-        if(isMoving)
+
+        if (isMoving)
         {
             MoveObject();
         }
-        */
-        
-        
-        
     }
-    
+
     void SetTarggetPosition()
     {
         Plane plane = new Plane(Vector3.up,transform.position);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         float point = 0f;
- 
+
         if(plane.Raycast(ray, out point))
             targetPos = ray.GetPoint(point);
-         
+
         isMoving = true;
     }
     void MoveObject()
     {
         transform.LookAt(targetPos);
         transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
- 
+
         if (transform.position == targetPos)
             isMoving = false;
         Debug.DrawLine(transform.position,targetPos,Color.red);
- 
+
     }
 }
