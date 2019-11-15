2af12722b7457afb24c320ad710f43f443b6f7c4
Luca Tescari <luca.tescari@bitforge.ch>
2019-11-15 19:36:44 +0100

    Updated SoccerField with goal mechanics (triggers)

---
+++ YBUnity/Assets/Scripts/Goal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public GoalType GoalType;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("ball"))
+        {
+            if (GoalType == GoalType.Opponent)
+            {
+                Debug.Log("GOAL AYYYY");
+            }
+            else if (GoalType == GoalType.Home)
+            {
+                Debug.Log("Neeeein, eigentor!");
+            }
+        }
+    }
+}
---
+++ YBUnity/Assets/Scripts/GoalType.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace
+{
+    public enum GoalType
+    {
+        Home, Opponent
+    }
+}
